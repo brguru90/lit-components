53a1286d9309fcd45c054d11b75180f59aaa8f96
Guruprasad BR <brguru90@gmail.com>
2024-06-18 22:01:18 +0530

    Guru (#6)
    * .
    
    * reactivity example
    
    ---------
    
    Co-authored-by: Guruprasad BR <guruprasad.br@esentire.com>

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,5 +1,6 @@
-import { LitElement, html,unsafeCSS } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { LitElement, html, unsafeCSS } from 'lit'
+import { customElement, state } from 'lit/decorators.js'
+import type { ButtonSizeType } from "./components/Button"
 
 import styles from '@/my-element.scss?inline';
 
@@ -9,15 +10,22 @@ import "./index"
 export class MyElement extends LitElement {
   static styles = unsafeCSS(styles);
 
+  @state()
+  selectedSize: ButtonSizeType = "md"
 
   render() {
     return html`
-      <vg-button size='sm'  @tick=${(e:Event)=>console.log(e)}>
+      <vg-button size="${this.selectedSize}"  @tick=${(e: Event) => console.log(e)}>
         Test
-      </vg-button><br /><br />
+      </vg-button>
+      
+      <select @change=${(e: Event) => this.selectedSize=e.target!.value}>
+        <option value="lg" ?selected=${this.selectedSize=="lg"}>lg</option>
+        <option value="md" ?selected=${this.selectedSize=="md"}>md</option>
+        <option value="sm" ?selected=${this.selectedSize=="sm"}>sm</option>
+      </select><br /><br />
 
       <vg-button></vg-button>
-      
       `
   }
 
